e213a0a20bf2bca27a62054c3bbfddf6430664bc
Anoopjoshy254 <anoopjoshy254@gmail.com>
2026-06-09 16:21:24 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -49,6 +49,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
 app.UseCors("AllowAngular");
 app.UseSwagger();
 app.UseSwaggerUI();
